6226495751ad67661918853cbf40558f22ffc043
Ahmed Shawkey <ahmed99shawkey@gmaill.com>
2025-01-12 19:34:59 +0200

    creating serilizer for Account model, and creating API view create a new Account and get all accounts

--- account_transfer/APIs/views.py
+++ account_transfer/APIs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Account
+from .serializers import AccountSerializer
+
+
 
 # Create your views here.
+
+
+class AccountListCreate(generics.ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
